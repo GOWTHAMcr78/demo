07181a7d5040599d53ef24e13732657663f36d82
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-16 11:41:37 +0530

    d6 post assignment

---
+++ PostAssignment/SLL_d6.java
@@ -0,0 +1,96 @@
+package PostAssignment;
+
+public class SLL_d6 {
+
+	Node top;
+	class Node
+	{
+		int data;
+		Node next;
+		
+		Node(int data)
+		{
+			this.data=data;
+			next=null;
+		}
+	}
+
+	SLL_d6()
+	{
+		top=null;
+	}
+
+	public void push(int data)
+	{
+		Node newnode=new Node(data);
+		if(top==null)
+		{
+			top=newnode;
+		}
+		else
+		{
+			newnode.next=top;
+			top=newnode;
+		}
+	}
+
+	public void pop()
+	{
+		if(top==null)
+		{
+			System.out.println("stack underflow");
+		}
+		else
+		{
+			int tmp=top.data;
+			top=top.next;
+			System.out.println("popped data "+tmp);
+		}
+	}
+
+	public void peak()
+	{
+		if(top==null)
+		{
+			System.out.println("stack is empty");
+		}
+		else
+		{
+			System.out.println("peak "+ top.data);
+		}
+	}
+
+	public void display()
+	{
+		Node node=top;
+		
+		while(node!=null)
+		{
+			System.out.println(node.data);
+			node=node.next;
+		}
+	}
+		public static void main(String[] args) {
+			SLL_d6 stack=new SLL_d6();
+			stack.push(10);
+			stack.push(20);
+			stack.push(30);
+			stack.push(40);
+			stack.push(50);
+			stack.display();
+			System.out.println();
+			
+			stack.pop();
+			stack.pop();
+			stack.display();
+			System.out.println();
+			
+			stack.peak();
+			
+
+		}
+
+
+}
+
+
